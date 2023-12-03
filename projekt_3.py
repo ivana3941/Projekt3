3a64375c9f34948f6130028a2dc1b00c3f269b7f
Ivana 3941 <bytypr@gmail.com>
2023-12-03 13:14:39 +0100

    oprava

--- projekt_3.py
+++ projekt_3.py
@@ -13,21 +13,25 @@ import requests
 import sys
 from bs4 import BeautifulSoup as bs
 import pandas
+
 def response_server(url):
     """získává a analyzuje data z požadované adresy URL"""
     response = requests.get(url)
     soup = bs(response.content, "html.parser")
     return soup
+
 def locations_all(soup):
     """vyhledá názvy jednotlivých obcí"""
     locations_all = soup.find_all("td", {"class": "overflow_name"})
     locations = [location.text for location in locations_all]
     return locations
-def codes_cities(soup):
+
+def codes_all(soup):
     """načte kódy jednotlivých obcí"""
-    codes_cities = soup.find_all("td", {"class": "cislo"})
-    codes = [code.text for code in codes_cities]
+    codes_all = soup.find_all("td", {"class": "cislo"})
+    codes = [code.text for code in codes_all]
     return codes
+
 def parties_all(url_sub, codes):
     """vyhledá názvy jednotlivých politických stran"""
     for code in codes:
@@ -36,31 +40,34 @@ def parties_all(url_sub, codes):
         parties_soup = soup.find_all("td", {"class": "overflow_name", "headers": ["t1sa1 t1sb2", "t2sa1 t2sb2"]})
         parties = [party.text for party in parties_soup]
     return parties
-def data_collector(url_sub, codes, locations, parties):
+
+def data_join(url_sub, codes, locations, parties):
     """shromažďuje, třídí a doplňuje všechna data"""
-    registered = []
+    electors = []
     envelopes = []
     valid = []
     data_votes = []
     for code in codes:
         url = f"{url_sub}{code}"
         soup = response_server(url)
-        registered.append(soup.find("td", {"class": "cislo", "headers": "sa2"}).text.replace(" ", "").replace('\xa0', ''))
+        electors.append(soup.find("td", {"class": "cislo", "headers": "sa2"}).text.replace(" ", "").replace('\xa0', ''))
         envelopes.append(soup.find("td", {"class": "cislo", "headers": "sa3"}).text.replace(" ", "").replace('\xa0', ''))
         valid.append(soup.find("td", {"class": "cislo", "headers": "sa6"}).text.replace(" ", "").replace('\xa0', ''))
         votes = soup.find_all("td", {"class": "cislo", "headers": ["t1sa2 t1sb3", "t2sa2 t2sb3"]})
         votes_clear = [vote.text.replace(" ", "").replace('\xa0', '') for vote in votes]
         data_votes.append(votes_clear)
-        data_1 = {'Code': codes, 'Location': locations, 'Registered': registered, 'Envelopes': envelopes, 'Valid': valid}
+        data_1 = {'Code': codes, 'Location': locations, 'Registered': electors, 'Envelopes': envelopes, 'Valid': valid}
         data_2 = {a: [data_votes[b][c] for b in range(len(data_votes)) for c, d in enumerate(parties) if d == a] for a in parties}
         data_all = {**data_1, **data_2}
     return data_all
+
 def csv_maker(data_all, csv_file):
     """převede získaná data do CSV"""
     table = pandas.DataFrame.from_dict(data_all)
     print ("Zapisuji výsledky do souboru CSV! ")
     table.to_csv(csv_file, index=False)
     return table
+
 def arguments():
     """kontrola argumentů ke spuštění programu"""
     if len(sys.argv) != 3:
@@ -74,6 +81,7 @@ def arguments():
         sys.exit()
     else:
         print(f"Stahuji data z: {sys.argv[1]} ")
+
 def main():
     """Hlavní funkce"""
     arguments()
@@ -83,11 +91,12 @@ def main():
     url = sys.argv[1]
     url_sub = f"https://www.volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj=12&xobec="
     soup=response_server(url)
-    codes=codes_cities(soup)
+    codes=codes_all(soup)
     locations=locations_all(soup)
     parties=parties_all(url_sub, codes)
-    data_all=data_collector(url_sub, codes, locations, parties)
+    data_all=data_join(url_sub, codes, locations, parties)
     csv_maker(data_all,csv_file)
     print(f"Data uložena do souboru: {csv_file}. Ukončuji program! ")
+    
 if __name__ == '__main__':
     main()
